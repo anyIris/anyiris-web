be340af595d9a7adb368b77c078f6324123d66f3
anyiris <dlwldns0112@gmail.com>
2020-01-24 21:05:22 +0900

    [mod] init project

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        @yield('meta')
+        <title>@yield('title', env('APP_NAME'))</title>
+
+        {{-- scripts --}}
+        @stack('before_scripts')
+
+        {{-- fonts --}}
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        {{-- styles --}}
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+        @yield('styles')
+    </head>
+    <body class="@yield('page', '')">
+        <div id="app">
+            @include('frontend.layouts.header')
+            <div class="container">
+                @yield('content')
+            </div>
+        </div>
+        @stack('after_scripts')
+    </body>
+</html>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<header class="site_header">
+    <div class="container">
+        <div class="title_area">
+            <h1><a href="#">ANYIRIS</a></h1>
+            <p class="title_description">BLOG &amp; PROJECT</p>
+        </div>
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">POST</a></li>
+                    <li><a href="#">PROJECT</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
